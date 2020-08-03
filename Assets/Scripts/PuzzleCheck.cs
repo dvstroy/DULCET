fe41e5c03ad3376cca008974d3fadfb5a687b502
Connor <Connorhayman99@gmail.com>
2020-08-04 11:43:28 +1200

    goop

---
+++ Assets/Scripts/PuzzleCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleCheck : MonoBehaviour
+
+       
+{    
+    void OnCollisionEnter(Collision puzzleBox)
+    {
+        Debug.Log("Puzzle 2 Complete");
+        Destroy(this);
+    }
+}
