6e05fdd462d388177b38169d3b6afcfeb1e65a75
Connor <Connorhayman99@gmail.com>
2020-08-05 11:53:46 +1200

    camera!!!!

---
+++ Assets/BaseCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCamera : MonoBehaviour
+{
+    public Transform m_Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HandleCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCamera();
+    }
+
+    protected virtual void HandleCamera()
+    {
+        if(!m_Target)
+        {
+            return;
+        }
+    }
+}
---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : BaseCamera
+{
+    public float m_Distance = 10f;
+    public float m_Height = 5f;
+    public float m_SmoothSpeed = 2f;
+
+    private Vector3 wantedPosition;
+    private Vector3 wantedBack;
+    private float wantedYAngle;
+
+    protected override void HandleCamera()
+    {
+        base.HandleCamera();
+
+        wantedYAngle = Mathf.LerpAngle(wantedYAngle, m_Target.eulerAngles.y, Time.deltaTime * m_SmoothSpeed);
+
+        Vector3 back = Vector3.back;
+        back = Quaternion.AngleAxis(wantedYAngle, Vector3.up) * back;
+
+        wantedPosition = (back * m_Distance) + (Vector3.up * m_Height) + m_Target.position;
+
+        transform.position = wantedPosition;
+        transform.LookAt(m_Target);
+    }
+}
