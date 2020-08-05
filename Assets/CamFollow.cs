df5180eb5ddd4bf0a7dd9a3deac66a561c370fee
Connor <Connorhayman99@gmail.com>
2020-08-06 11:48:20 +1200

    carrot

---
+++ Assets/CamFollow.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public GameObject followTarget, myCam;
+    public float lerpSpeed, camXSensitivity, camYSensitivity;
+    public Movement player;
+    public float maxMouseHeight = 50;
+    public float minMouseHeight = -50;
+    public float currentMouseHeight;
+
+    private void Start()
+    {
+       // currentMouseHeight = 0;
+       // currentMouseHeight += Input.GetAxis("Mouse Y");
+    }
+
+    void Update()
+    {
+        player.SetCameraForward(myCam.transform.forward);
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * camXSensitivity, 0));
+        var currentMouseChange = Input.GetAxis("Mouse Y");
+        var intendedMouseHeight = currentMouseChange + currentMouseHeight;
+
+        if (intendedMouseHeight > maxMouseHeight || intendedMouseHeight < minMouseHeight)
+        {
+            var extra = 0f;
+            if (intendedMouseHeight > maxMouseHeight)
+            {
+                extra = intendedMouseHeight - maxMouseHeight;
+            }
+            if (intendedMouseHeight < minMouseHeight)
+            {
+                extra = intendedMouseHeight - minMouseHeight;
+            }
+            currentMouseChange -= extra;
+        }
+
+
+
+        //myCam.transform.Rotate(new Vector3(currentMouseChange * -camYSensitivity, 0, 0));
+        //myCam.transform.Translate(new Vector3(0, currentMouseChange * -0.1f, 0), Space.World);
+        //currentMouseHeight += currentMouseChange;
+
+        //transform.position = Vector3.Lerp(transform.position, followTarget.transform.position, lerpSpeed);
+
+
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,18 +1,143 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public KeyCode forward, backward, left, right, jump, musicMenu;
+    public float jumpForce;
+    public float runSpeed;
+    public bool isGrounded;
+
+    Animator anim;
+    Rigidbody rb;
+    Vector3 wantedDirection, camDirection;
+    bool wantedJump;
+    Vector3 debugEulerAngles;
+    Vector3 resetPos = Vector3.zero;
+    float cameraRotationY = 0;
+    bool cursorVisible = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        Cursor.visible = cursorVisible;
+        Cursor.lockState = CursorLockMode.Locked;
+        isGrounded = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //R Resets player to 000
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+
+        //Alt + R resets the entire scene to it's default state
+        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    Cursor.visible = !cursorVisible;
+        //    cursorVisible = !cursorVisible;
+        //}
+
+        //Setting up vars for movement
+        var forwardIntent = 0;
+        var horizontalIntent = 0;
+
+        //Taking inputs
+        if (Input.GetKey(forward))
+        {
+            forwardIntent += 1;
+        }
+        if (Input.GetKey(backward))
+        {
+            forwardIntent -= 1;
+        }
+        if (Input.GetKey(left))
+        {
+            horizontalIntent -= 1;
+        }
+        if (Input.GetKey(right))
+        {
+            horizontalIntent += 1;
+        }
+        //Storing the direction the player wants to go in
+        wantedDirection = new Vector3(horizontalIntent, 0, forwardIntent).normalized;
+
+        //Taking jump input
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            wantedJump = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        //This if checks if the player is moving
+        if (wantedDirection.magnitude == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            //The code that references where the camera is and how that should effect the player
+            rb.transform.forward = Quaternion.Euler(0, cameraRotationY, 0) * wantedDirection;
+            //The code that actually moves the palyer
+            rb.MovePosition(rb.position + rb.transform.forward * runSpeed * Time.deltaTime);
+            anim.SetBool("isRunning", true);
+        }
+
+
+        if (wantedJump)
+        {
+            //Makes the player jump
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            anim.SetTrigger("jump");
+            wantedJump = false;
+        }
+
+    }
+
+    //For the camera script to tell the player which way the camera is facing, purely on the Y rotaitonal axis
+    public void SetCameraForward(Vector3 forward)
+    {
+        cameraRotationY = Quaternion.FromToRotation(Vector3.forward, new Vector3(forward.x, 0, forward.z)).eulerAngles.y;
+    }
+
+    //For other scripts to set the player to be grounded
+    public void SetGrounded(bool grounded)
+    {
+        isGrounded = grounded;
+        anim.SetBool("inAir", !grounded);
+    }
+
+    void Reset()
+    {
+        transform.position = resetPos;
+        rb.velocity = new Vector3(0, 0, 0);
+    }
+
+    public void UnlockCursor()
+    {
+        Cursor.visible = !cursorVisible;
+        cursorVisible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LockCursor()
+    {
+        Cursor.visible = !cursorVisible;
+        cursorVisible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
