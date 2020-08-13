5f93367d0e0bd0d4e3460fe956d4f3575942c97d
Christa <cmlhc99@gmail.com>
2020-08-14 11:31:21 +1200

    Animations

---
+++ Assets/Scripts/AniScript/PC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horisontal", Input.GetAxis("Horisontal"));
+    }
+}
